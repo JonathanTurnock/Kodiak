3b612efd88ad9af4546e33072b035b64c7f89ef9
bitbucket-pipelines <commits-noreply@bitbucket.org>
2019-06-11 19:39:38 +0000

    Bump version: 0.0.7 → 0.0.8-SNAPSHOT

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='fxq-ae-runner',
-    version='0.0.7',
+    version='0.0.8-SNAPSHOT',
     packages=[
         'fxq.ae.runner',
         'fxq.ae.runner.marshaller',
