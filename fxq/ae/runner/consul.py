faf9650017886495bb1d6f5582b8bdda3c7aeea1
Jonathan Turnock <jonathan.turnock@fxquants.net>
2019-07-17 00:09:34 +0100

    Initial Consul Service Registration

---
+++ fxq/ae/runner/consul.py
@@ -0,0 +1,56 @@
+import requests
+
+# r = requests.get('http://localhost:8500/v1/catalog/nodes')
+# first_node = r.json()[0]
+# print(first_node["Node"])
+
+
+# service_def = {
+#     "ID": "ae-runner-5000",
+#     "Service": "ae-runner",
+#     "Tags": [
+#       "secure=false"
+#     ],
+#     "Address": "172.16.0.101",
+#     "Meta": {},
+#     "Port": 5000
+#   }
+#
+# service_check = {
+#     "Node": first_node["Node"],
+#     "CheckID": "service:ae-runner-5000",
+#     "Name": "Service 'ae-runner' check",
+#     "Status": "passing",
+#     "ServiceID": "ae-runner-5000",
+#     "Definition": {
+#       "Http": "http://172.16.0.101:5000/api/health",
+#       "Interval": "15s",
+#       "Timeout": "5s",
+#       "DeregisterCriticalServiceAfter": "120s"
+#     }
+# }
+
+service_def = {
+  "ID": "service:ae-runner-5000",
+  "Name": "ae-runner",
+  "Tags": ["secure=false"],
+  "Address": "172.16.0.101",
+  "Port": 5000,
+  "Meta": {},
+  "EnableTagOverride": False,
+  "Check": {
+    "DeregisterCriticalServiceAfter": "60s",
+    "HTTP": "http://172.16.0.101:5000/api/health",
+    "Interval": "10s"
+  },
+  "Weights": {
+    "Passing": 10,
+    "Warning": 1
+  }
+}
+
+# first_node["Service"] = service_def
+# first_node["Check"] = service_check
+
+r = requests.put('http://localhost:8500/v1/agent/service/register', json=service_def)
+print(r.text)
